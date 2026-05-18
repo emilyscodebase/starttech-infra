36df3b3bf46b9b0fdfbacbd702a49da7b10b7872
Promise Uchechukwu <promiseuchechukwu70@gmail.com>
2026-05-18 05:36:02 +0100

    Fix Redis security group reference

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -176,7 +176,7 @@ resource "aws_security_group" "redis" {
     from_port       = 6379
     to_port         = 6379
     protocol        = "tcp"
-    security_groups = [aws_security_group.ec2.id]
+    security_groups = [var.ec2_security_group_id]
   }
 
   egress {
